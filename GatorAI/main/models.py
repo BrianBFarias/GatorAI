6edd5d26dc30733ee10f2b6e37cd407b1706ae4a
Brian Farias <Brian03032003@gmail.com>
2023-10-13 21:07:03 -0400

    some updates

--- GatorAI/main/models.py
+++ GatorAI/main/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
 
+# class Product(models.Model):
+#     pass
