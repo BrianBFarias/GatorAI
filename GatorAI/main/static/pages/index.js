b009b06a0720cd368319089b7e60658b7b299354
Brian Farias <Brian03032003@gmail.com>
2023-10-14 21:04:56 -0400

    update

--- GatorAI/main/static/pages/index.js
+++ GatorAI/main/static/pages/index.js
@@ -5,5 +5,17 @@ function load(){
 }
 
 function AIsearch(){
+	fetch(`/posts?start=${start}&end=${end}&spice=${localStorage.getItem('filterS') }&flavor=${localStorage.getItem('filterF') }`)
+    .then(response => response.json())
+    .then(async data => {
+        if(!data){
 
+        }
+        else{
+
+          for (const product of data) {
+			
+          }
+        }
+      });
 }
